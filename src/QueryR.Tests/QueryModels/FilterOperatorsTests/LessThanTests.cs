10668128fceb9cfe6004abbffec3df5bea13e14d
Craig McCauley <craig.mccauley@gmail.com>
2024-06-01 17:52:10 +0100

    clean up

--- src/QueryR.Tests/QueryModels/FilterOperatorsTests/LessThanTests.cs
+++ src/QueryR.Tests/QueryModels/FilterOperatorsTests/LessThanTests.cs
@@ -5,8 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace QueryR.Tests.QueryModels.FilterOperatorsTests;
